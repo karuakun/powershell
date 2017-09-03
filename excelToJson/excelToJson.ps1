cc234e297bade5fae2869e0e47006f7a66774f82
karuakun <karuakun@gmail.com>
2017-09-03 19:22:49 +0900

    サンプル完成

---
+++ excelToJson/excelToJson.ps1
@@ -0,0 +1,24 @@
+﻿Param (
+    [parameter(mandatory=$true)] [string]$inExcelFilePath,
+    [parameter(mandatory=$true)] [string]$outJsonFilePath,
+    $worksheetNameOrNo = 1
+)
+
+if (! (Test-Path $inExcelFilePath)) {
+    "ファイル:{0}が存在しません。" -f $inExcelFilePath
+    return
+}
+
+Add-Type -Path (Join-Path $PSScriptRoot ClosedXML.dll)
+$book = New-Object ClosedXML.Excel.XLWorkbook($inExcelFilePath);
+$data = foreach($row in $book.Worksheet($worksheetNameOrNo).RangeUsed().AsTable().Rows()) {
+    foreach($cell in $row.Cells()) {
+        $prop = @{
+            Address = $cell.Address.ColumnNumber.ToString() + ":" + $cell.Address.RowNumber.ToString()
+            Value =  $cell.Value 
+        }
+        New-Object PSObject -Property $prop
+    }
+}
+$data | select address, value | ConvertTo-Json | Out-File $outJsonFilePath
+
