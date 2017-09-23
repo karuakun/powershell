a7b1f959ca3f1cc7edd92df5c5f25cf1bdf43c5f
karuakun <karuakun@gmail.com>
2017-09-23 19:33:21 +0900

    logStatistics.ps1 added

---
+++ gitTool/logStatistics.ps1
@@ -0,0 +1,14 @@
+﻿"repository";
+git remote -v ;
+""
+"branch"
+git branch | where {$_.startswith("*")};
+git log --pretty=format:%h,%ae,%ad --date=short --since =1.month `
+| convertfrom-csv -header hash, user, date `
+| group date, user -NoElement `
+| select `
+    @{ Name="User"; Expression = { $_.Name.Split()[1].Trim(",")}}, `
+    @{ Name="Date"; Expression = { $_.Name.Split()[0]}}, `
+    Count `
+| Sort User , Date;
+cd 
